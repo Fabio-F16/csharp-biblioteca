098e54b7bbbe36132284212693b6b144adba9ac3
Fabio-F16 <fipsfabio95@gmail.com>
2022-06-30 17:06:57 +0200

    Parziale

---
+++ DVD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class DVD : Documento
+    {
+        public int numeroSeriale;
+        public double durata;
+
+        public DVD(string titolo, int anno, string autoreNome, string autoreCognome, int numeroSeriale, double durata) : base(titolo, anno, autoreNome, autoreCognome)
+        {
+            this.numeroSeriale = numeroSeriale;
+            this.durata = durata;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n" + "Numero seriale: " + this.numeroSeriale + "\n" + "Durata: " + this.durata + "\n" + "--------------------";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Console.WriteLine("Hello, World!");
+
+
+            // utente registrazione...?
+            Console.WriteLine("Inserisci i dati per accedere");
+            Console.WriteLine();
+            Console.WriteLine("Inserisci il tuo nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Inserisci il tuo cognome");
+            string cognome = Console.ReadLine();
+
+            Console.WriteLine("Inserisci la tua email");
+            string email = Console.ReadLine();
+
+            Utente utente1 = new Utente(nome, cognome, email);
+            
+            // libri istanziati
+            Libro libro1 = new Libro("Il signore degli anelli" , 2002, "Claudio", "Bisio", "ISBN 544 5 55 44444 4", 212);
+           // Console.WriteLine(libro1.ToString());
+            Libro libro2 = new Libro("Bibbia", 0000, "Antico", "Scrittore", "ISBN 544 5 55 44444 4", 1432);
+            // Console.WriteLine(libro2.ToString());
+
+
+            // dvd istanziati
+            DVD dvd1 = new DVD("Gli incredibili", 2012, "Vassaver", "Vassaver", 45545454, 2.13);
+
+
+
+            Biblioteca biblioteca = new Biblioteca();
+            biblioteca.AggiungiUtente(utente1);
+            biblioteca.AggiungiDocumento(libro1);
+            biblioteca.AggiungiDocumento(libro2);
+            biblioteca.AggiungiDocumento(dvd1);
+
+            biblioteca.StampaDocumento();
+            biblioteca.StampaUtenti();
         }
     }
 }
